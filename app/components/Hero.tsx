7f2079254577ac824f717e3a96f678b1c5ab85b0
Liya Bekyarova <liya.ilieva@gmail.com>
2025-05-30 09:51:23 +0300

    hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useEffect, useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils";
+
+export const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(heroVideo);
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 768) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    handleVideoSrcSet();
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      ease: "power2.inOut",
+      delay: 2,
+    });
+
+    gsap.to("#cta", {
+        opacity: 1,
+        y:-50,
+        ease: "power2.inOut",
+        delay: 2,
+    });
+
+  });
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            autoPlay
+            muted
+            playsInline
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source src={videoSrc} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl ">From $199 or $999</p>
+      </div>
+    </section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import { Hero } from "./components/Hero";
+import { Highlights } from "./components/Highlights";
+import { NavBar } from "./components/NavBar";
+
 const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-blue common-padding flex-center ">Hello there!</h1>
-
-      <div className=" nav-height">
-        <button>Click Me</button>
-      </div>
-    </div>
+   <main className="bg-black">
+<NavBar/>
+<Hero/>
+<Highlights/>
+   </main>
   );
 };
 export default Home;
